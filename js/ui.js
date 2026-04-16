e2611dc055653a873a7facf3fffbaee5e17d158a
Angel <perez.angelo71@gmail.com>
2026-04-16 21:11:47 +0200

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,14 @@
+function loadPartial(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadPartial("header", "/partials/header.html");
+  loadPartial("ticker", "/partials/ticker.html");
+  loadPartial("sidebar", "/partials/sidebar.html");
+  loadPartial("footer", "/partials/footer.html");
+});
