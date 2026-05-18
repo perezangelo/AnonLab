3d3ec17443fb5ebdd65ec94e6f12f5fda0fb7ed3
Angel <perez.angelo71@gmail.com>
2026-05-18 13:05:46 +0200

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbFile = __DIR__ . '/database/users.db';
-
-try {
-    $db = new PDO('sqlite:' . $dbFile);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Errore connessione database.");
-}
