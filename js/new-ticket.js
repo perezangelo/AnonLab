9723f0d7651bb6cb51d409f3b044609a46748ee9
Angel <perez.angelo71@gmail.com>
2026-05-24 16:26:48 +0200

    Update new-ticket.js

--- js/new-ticket.js
+++ js/new-ticket.js
@@ -1,9 +1,5 @@
 <form id="register-form" action="https://angelonline.altervista.org/send-ticket.php" method="POST">
   <!-- Web3Forms -->
-  <input type="hidden" name="access_key" value="9d55bc37-731e-405c-b3c2-b24c3feffb8c">
-  <input type="hidden" name="subject" value="Nuova Registrazione Utente - AnonLab">
-  <input type="hidden" name="from_name" value="AnonLab">
-  <input type="hidden" name="redirect" value="https://anonlab.it/grazie.html">
 
   <!-- Honeypot -->
   <input type="checkbox" name="botcheck" style="display:none;">
