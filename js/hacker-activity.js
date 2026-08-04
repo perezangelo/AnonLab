0a5ba57646b18cac46847505dd1305c9a2bdbf1f
Angel <perez.angelo71@gmail.com>
2026-08-04 20:14:54 +0200

    Create hacker-activity.js

---
+++ js/hacker-activity.js
@@ -0,0 +1,14 @@
+function initHackerActivity() {
+    const box = document.getElementById("hacker-activity");
+    if (!box) return;
+
+    fetch("/backend/hacker-activity.php?ts=" + Date.now())
+        .then(response => response.text())
+        .then(html => {
+            box.innerHTML = html;
+        })
+        .catch(err => {
+            box.innerHTML = "<p style='color:#ff7b00;'>Errore caricamento dati CTI</p>";
+            console.error("CTI error:", err);
+        });
+}
