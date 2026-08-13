66c0b59ff15a5af49086d071ac17a4ee6872a9b1
Angel <perez.angelo71@gmail.com>
2026-08-13 10:44:00 +0200

    Update RSS feed URLs in home-news.js

--- js/home-news.js
+++ js/home-news.js
@@ -11,10 +11,10 @@ async function loadHomeNews() {
 
     // FEED MULTIPLI (via rss2json)
     const feeds = [
-        "https://api.rss2json.com/v1/api.json?rss_url=https://feeds.feedburner.com/TheHackersNews",
-        "https://angelonline.altervista.org/api/hackread.php",
-        "https://api.rss2json.com/v1/api.json?rss_url=https://www.darkreading.com/rss.xml"
-    ];
+    "https://angelonline.altervista.org/api/hackernews.php",
+    "https://angelonline.altervista.org/api/hackread.php",
+    "https://angelonline.altervista.org/api/darkreading.php"
+];
 
     // Link esterni personalizzati (in ordine)
     const externalLinks = [
