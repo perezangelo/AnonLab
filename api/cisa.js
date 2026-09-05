acec31a84e7c67ae37f3a609de1e2241ab136468
Angel <perez.angelo71@gmail.com>
2026-09-05 19:49:50 +0200

    Update cisa.js

--- api/cisa.js
+++ api/cisa.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const url = "https://www.cisa.gov/news.xml";
 
   try {
